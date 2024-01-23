e15a849d83c083ffe19aa3a91eae46ddf24ca76a
mmilinda <mmilinda00@gmail.com>
2024-01-23 16:16:52 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import LoginSignup from './Components/LoginSignup/LoginSignup'; // Assurez-vous que le chemin est correct
+
+
+function App() {
+  return (
+    <div>
+        <LoginSignup/>
+    </div>
+  );
+}
+
+export default App;
